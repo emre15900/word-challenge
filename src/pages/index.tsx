e9e5fbe59b0cc8581ed442fe05ac691904cb9397
emre15900 <emresayinweb@gmail.com>
2024-01-06 18:52:15 +0300

    handleFormSubmit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,6 +43,10 @@ function HomePage() {
     setOtp("");
   }, [currentPage]);
 
+  const handleFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
   const notify = (answer: string) => {
     toast(
       otp === answer
@@ -70,11 +74,11 @@ function HomePage() {
     background: "#00e800",
     outline: 0,
     border: "1px solid rgb(4 201 4)",
-    padding: "15px 5rem",
+    padding: "12px 4rem",
     fontWeight: 400,
     fontSize: "17px",
     cursor: "pointer",
-    marginTop: "30px",
+    marginTop: "20px",
     transition: "background 0.3s ease",
     color: "#000",
   });
@@ -103,136 +107,72 @@ function HomePage() {
         Word Challenge
       </h1>
       <hr />
-      <div
-        key={currentWord.id}
-        style={{
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: 10,
-        }}
-      >
-        <div style={{ width: 350 }}>
-          <img
-            src={currentWord.img}
-            alt={currentWord.word}
-            style={{
-              borderRadius: "30px",
-              marginTop: "30px",
-              boxShadow: "0 0 10px 0 #000000",
-              width: "100%",
-            }}
-          />
-        </div>
-        <div>
-          <h1>{currentWord.word}</h1>
-        </div>
-        <div className={styles.otpInput}>
-          <OtpInput
-            value={otp}
-            onChange={setOtp}
-            numInputs={currentWord.answer.length}
-            renderSeparator={<span>-</span>}
-            renderInput={(props) => <input {...props} />}
-            inputType="text"
-            shouldAutoFocus
-            inputStyle={{
-              width: "50px",
-              height: "50px",
-              margin: "0 10px",
-              fontSize: "20px",
-              borderRadius: 50,
-              border: "1px solid rgba(0,0,0,0.3)",
-              fontWeight: 800,
-            }}
-          />
-        </div>
-        <div>
-          <button
-            style={buttonStyle}
-            onMouseOver={handleHover}
-            onMouseLeave={handleMouseLeave}
-            onClick={() => notify(currentWord.answer)}
-          >
-            Verify
-          </button>
-          <ToastContainer />
-        </div>
-        <div>
-          <p style={{ fontWeight: 500 }}>
-            <strong>{currentWord.id}</strong> / {words.length}
-          </p>
-        </div>
-      </div>
-      {/* {words.map((word) => {
-        return (
-          <div
-            key={word.id}
-            style={{
-              width: "100%",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              gap: 10,
-            }}
-          >
-            <div style={{ width: 350 }}>
-              <img
-                src={word.img}
-                alt={word.word}
-                style={{
-                  borderRadius: "30px",
-                  marginTop: "30px",
-                  boxShadow: "0 0 10px 0 #000000",
-                  width: "100%",
-                }}
-              />
-            </div>
-            <div>
-              <h1>{word.word}</h1>
-            </div>
-            <div className={styles.otpInput}>
-              <OtpInput
-                value={otp}
-                onChange={setOtp}
-                numInputs={word.answer.length}
-                renderSeparator={<span>-</span>}
-                renderInput={(props) => <input {...props} />}
-                inputType="text"
-                // shouldAutoFocus
-                inputStyle={{
-                  width: "50px",
-                  height: "50px",
-                  margin: "0 10px",
-                  fontSize: "20px",
-                  borderRadius: 50,
-                  border: "1px solid rgba(0,0,0,0.3)",
-                  fontWeight: 800,
-                }}
-              />
-            </div>
-            <div>
-              <button
-                style={buttonStyle}
-                onMouseOver={handleHover}
-                onMouseLeave={handleMouseLeave}
-                onClick={() => notify(currentWord.answer)}
-              >
-                Verify
-              </button>
-              <ToastContainer />
-            </div>
-            <div>
-              <p style={{ fontWeight: 500 }}>
-                <strong>{currentWord.id}</strong> / {words.length}
-              </p>
-            </div>
+      <form onSubmit={handleFormSubmit}>
+        <div
+          key={currentWord.id}
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 10,
+          }}
+        >
+          <div style={{ width: 350 }}>
+            <img
+              src={currentWord.img}
+              alt={currentWord.word}
+              style={{
+                borderRadius: "30px",
+                marginTop: "30px",
+                boxShadow: "0 0 10px 0 #000000",
+                width: "100%",
+              }}
+            />
           </div>
-        );
-      })} */}
+          <div>
+            <h1>{currentWord.word}</h1>
+          </div>
+          <div className={styles.otpInput}>
+            <OtpInput
+              value={otp}
+              onChange={setOtp}
+              numInputs={currentWord.answer.length}
+              renderSeparator={<span>-</span>}
+              renderInput={(props) => <input {...props} />}
+              inputType="text"
+              shouldAutoFocus
+              inputStyle={{
+                width: "42px",
+                height: "42px",
+                margin: "0 10px",
+                fontSize: "20px",
+                borderRadius: 80,
+                border: "1px solid rgba(0,0,0,0.3)",
+                fontWeight: 800,
+              }}
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              style={buttonStyle}
+              onMouseOver={handleHover}
+              onMouseLeave={handleMouseLeave}
+              onClick={() => notify(currentWord.answer)}
+            >
+              Verify
+            </button>
+            <ToastContainer />
+          </div>
+          <div>
+            <p style={{ fontWeight: 500 }}>
+              <strong>{currentWord.id}</strong> / {words.length}
+            </p>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
