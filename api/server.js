3cc9266e7468a5deea986904accc583138c1b802
emre15900 <emresayinweb@gmail.com>
2024-03-19 17:04:53 +0300

    Connected to MongoDB

--- api/server.js
+++ api/server.js
@@ -3,20 +3,25 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
+const WordModel = require("./models/WordModel");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/test", (req, res) => {
-  res.send("<h1>Test Page!</h1>");
-});
+// Connect to MongoDB
+mongoose
+  .connect(
+    "mongodb+srv://emresayin355:aUXq3UfiTJ31Uxf1@cluster0.x6cr7m6.mongodb.net/?retryWrites=true&w=majority&appName=words"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(4000, () => {
-  console.log("Server has started on port 5000");
+  console.log("Server has started on port 4000");
 });
