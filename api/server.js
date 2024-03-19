c669fd5437e013a64c5f6fa37a0d03d7e5dc8f19
emre15900 <emresayinweb@gmail.com>
2024-03-19 03:41:35 +0300

    create model

--- api/server.js
+++ api/server.js
@@ -13,6 +13,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/test", (req, res) => {
+  res.send("<h1>Test Page!</h1>");
+});
+
 app.listen(4000, () => {
   console.log("Server has started on port 5000");
 });
